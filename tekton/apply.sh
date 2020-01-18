5b7b1c2a391e02f8933824ff8ecc97d4f30bb364
Jason Zaman <jason@perfinion.com>
2020-01-18 22:58:22 +0800

    Migrate to new GCP project for RBE
    Signed-off-by: Jason Zaman <jason@perfinion.com>

--- tekton/apply.sh
+++ tekton/apply.sh
@@ -11,11 +11,23 @@ kubectl apply -f namespace.yaml
 kubectl apply -f role-admin.yaml
 kubectl apply -f role-webhook.yaml
 
-# Add GitHub webhook validator for trigger event-listener
-kubectl apply -f webhook-deployment.yaml
-
 # Tasks for tf/build repo
 kubectl apply -f build-tasks.yaml
 kubectl apply -f build-pipeline.yaml
-kubectl apply -f build-triggers.yaml
+
+# Initial cluster setup should not apply build-triggers.
+# Tne trigger event listener needs the validator image
+# built first so kick off a PipelineRun to build everything,
+# then apply the webhook and triggers later
+if [[ $1 == "--install" ]]; then
+    # PipelineRun to build the linxcpu build image and webhook validator
+    kubectl apply -f build-pipeline-run.yaml
+
+else
+    # Add GitHub webhook validator for trigger event-listener
+    kubectl apply -f webhook-deployment.yaml
+
+    # Triggers and ingress for GitHub webhook
+    kubectl apply -f build-triggers.yaml
+fi
 
--- tekton/install.sh
+++ tekton/install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-: ${PROJECT:=perfinion}
+: ${PROJECT:=tensorflow-build-224104}
 : ${CLUSTER_NAME:=tf-build-dev}
 : ${CLUSTER_REGION:=us-central1}
 : ${CLUSTER_ZONE:=${CLUSTER_REGION}-a}
 
 : ${TKN_PIPELINE_VERSION:=v0.9.0}
 : ${TKN_TRIGGERS_VERSION:=v0.1.0}
+: ${TKN_DASHBOARD_VERSION:=v0.4.0}
 
 # Check if cluster is running
-gcloud container clusters describe ${CLUSTER_NAME} --zone=${CLUSTER_ZONE}
+gcloud container clusters describe ${CLUSTER_NAME} --project=${PROJECT} --zone=${CLUSTER_ZONE} >/dev/null
 if [[ $? -eq 1 ]]; then
     echo "ERROR: GKE cluster does not exist" >&2
     echo "" >&2
@@ -20,37 +21,43 @@ if [[ $? -eq 1 ]]; then
         echo "Quitting ..." >&2
         exit 1
     fi
+
+    set -e
+
+    # Create GKE cluster
+    gcloud beta container clusters create ${CLUSTER_NAME} \
+        --project ${PROJECT} \
+        --zone ${CLUSTER_ZONE} \
+        --no-enable-basic-auth \
+        --release-channel "regular" \
+        --machine-type "n1-standard-2" \
+        --num-nodes "1" \
+        --image-type "COS" \
+        --disk-type "pd-standard" \
+        --disk-size "100" \
+        --metadata disable-legacy-endpoints=true \
+        --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
+        --enable-stackdriver-kubernetes \
+        --enable-vertical-pod-autoscaling \
+        --enable-ip-alias \
+        --network "projects/${PROJECT}/global/networks/default" \
+        --subnetwork "projects/${PROJECT}/regions/${CLUSTER_REGION}/subnetworks/default" \
+        --default-max-pods-per-node "110" \
+        --addons HorizontalPodAutoscaling,HttpLoadBalancing \
+        --enable-autoupgrade \
+        --enable-autorepair
+
+    # Make current user a cluster admin
+    kubectl create clusterrolebinding cluster-admin-binding \
+        --clusterrole=cluster-admin \
+        --user=$(gcloud config get-value core/account)
+
 fi
 
 set -e
 
-# Create GKE cluster
-gcloud container clusters create ${CLUSTER_NAME} \
-    --project ${PROJECT} \
-    --zone ${CLUSTER_ZONE} \
-    --no-enable-basic-auth \
-    --cluster-version "1.13.11-gke.14" \
-    --machine-type "n1-standard-1" \
-    --num-nodes "1" \
-    --image-type "COS" \
-    --disk-type "pd-standard" \
-    --disk-size "100" \
-    --metadata disable-legacy-endpoints=true \
-    --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
-    --enable-cloud-logging \
-    --enable-cloud-monitoring \
-    --enable-ip-alias \
-    --network "projects/${PROJECT}/global/networks/default" \
-    --subnetwork "projects/${PROJECT}/regions/${CLUSTER_REGION}/subnetworks/default" \
-    --default-max-pods-per-node "110" \
-    --addons HorizontalPodAutoscaling,HttpLoadBalancing \
-    --enable-autoupgrade \
-    --enable-autorepair
-
-# Make current user a cluster admin
-kubectl create clusterrolebinding cluster-admin-binding \
-    --clusterrole=cluster-admin \
-    --user=$(gcloud config get-value core/account)
+# get creds for kubectl
+gcloud container clusters get-credentials ${CLUSTER_NAME} --project=${PROJECT} --zone=${CLUSTER_ZONE}
 
 # Install tekton pipelines on new cluster
 kubectl apply -f https://storage.googleapis.com/tekton-releases/pipeline/previous/${TKN_PIPELINE_VERSION}/release.yaml
@@ -58,8 +65,13 @@ kubectl apply -f https://storage.googleapis.com/tekton-releases/pipeline/previou
 # Install tekton triggers on new cluster
 kubectl apply -f https://storage.googleapis.com/tekton-releases/triggers/previous/${TKN_TRIGGERS_VERSION}/release.yaml
 
+# Install tekton dashboard
+kubectl apply -f https://storage.googleapis.com/tekton-releases/dashboard/previous/${TKN_DASHBOARD_VERSION}/release.yaml
+
+# New cluster should have a 'tekton-pipelines' namespace and several tekton pods
 kubectl get pods --all-namespaces
 
-# Create sig-build namespace first
-kubectl apply -f namespace.yaml
+# Apply all the configs except triggers
+# Trigger needs the validator service image built first
+./apply.sh --install
 
