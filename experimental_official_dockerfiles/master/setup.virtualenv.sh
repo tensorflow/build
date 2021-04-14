c7811fea9520ffd156b114f557323ff578c24b31
Austin Anderson <angerson@google.com>
2021-04-14 15:22:25 -0700

    More WIP, still wont build

--- experimental_official_dockerfiles/master/setup.virtualenv.sh
+++ experimental_official_dockerfiles/master/setup.virtualenv.sh
@@ -4,7 +4,10 @@ set -e
 # Usage: setup_virtualenv.sh <pyversion> <virtualenv-name> <requirements.txt>
 # setup_virtualenv.sh 3.6.9 tf-36 requirements.txt
 source ~/.bashrc
-pyenv virtualenv $1 $2
-pyenv activate $2
+VERSION=$1
+NAME=$2
+REQUIREMENTS=$3
+pyenv virtualenv $VERSION $NAME
+pyenv activate $NAME
 pip install --upgrade pip
-pip install -r $3 -U
+pip install -r $REQUIREMENTS -U
