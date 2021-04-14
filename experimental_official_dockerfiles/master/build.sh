0b76d11f23a16b402b5461e9e3020f28af384afc
Austin Anderson <angerson@google.com>
2021-04-14 15:22:25 -0700

    More WIP, still wont build

--- experimental_official_dockerfiles/master/build.sh
+++ experimental_official_dockerfiles/master/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-mkdir -p ~/tmp/dockertests/cache
-DOCKER_BUILDKIT=1 docker build --target=runtime -t tf-exp-runtime .
-docker run --gpus=all --rm tf-exp-runtime python -c "import tensorflow as tf; tf.test.is_gpu_available()"
+# mkdir -p ~/tmp/dockertests/cache
+# DOCKER_BUILDKIT=1 docker build --target=runtime -t tf-exp-runtime .
+# docker run --gpus=all --rm tf-exp-runtime python -c "import tensorflow as tf; tf.test.is_gpu_available()"
 
-# DOCKER_BUILDKIT=1 docker build --target=devel -t tf-exp-devel .
-# docker run --rm -v ~/tmp/dockertests:/tmp/dockertests tf-exp-devel /build_tf.sh
+DOCKER_BUILDKIT=1 docker build --target=devel -t tf-exp-devel .
+docker run --rm -v ~/tmp/dockertests:/tmp/dockertests tf-exp-devel /devel.build_tf.sh
--- experimental_official_dockerfiles/master/devel.build_tf.sh
+++ experimental_official_dockerfiles/master/devel.build_tf.sh
@@ -2,8 +2,7 @@
 set -e
 source ~/.bashrc
 pyenv global tf-38
-git clone https://github.com/tensorflow/tensorflow --depth=1
-mv tf_configure.bazelrc tensorflow/.tf_configure.bazelrc
-cd tensorflow
+mv devel.bazelrc /tmp/dockertests/tensorflow/.tf_configure.bazelrc
+cd /tmp/dockertests/tensorflow
 bazel build tensorflow/tools/pip_package:build_pip_package --disk_cache=/tmp/dockertests/cache
 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/dockertests/pkg --gpu
