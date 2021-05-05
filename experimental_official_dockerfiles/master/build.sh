da49bcf4235465bb9d1be21d305547596688d715
Austin Anderson <angerson@google.com>
2021-05-05 15:36:44 -0700

    Tidy things up

--- experimental_official_dockerfiles/master/build.sh
+++ experimental_official_dockerfiles/master/build.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-# mkdir -p ~/tmp/dockertests/cache
 # DOCKER_BUILDKIT=1 docker build --target=runtime -t tf-exp-runtime .
 # docker run --gpus=all --rm tf-exp-runtime python -c "import tensorflow as tf; tf.test.is_gpu_available()"
 
-DOCKER_BUILDKIT=1 docker build --target=devel --build-arg PYENV_VERSION=3.9.1 -t tf-exp-devel .
+DOCKER_BUILDKIT=1 docker build --target=devel --build-arg PYTHON_VERSION=python3.8 -t tf-exp-devel .
 # docker run --rm -v ~/tmp/dockertests:/tmp/dockertests tf-exp-devel bats -T /devel.preliminary.bats
 
 # docker run --rm -it -v ~/tmp/dockertests:/tmp/dockertests tf-exp-devel bash
 
-docker run --rm -v ~/tmp/dockertests:/tmp/dockertests tf-exp-devel /devel.build_tf.sh
+docker run --rm -v ~/tmp/dockertests:/tf tf-exp-devel /devel.build_tf.sh
 
 # docker run --rm -v ~/tmp/dockertests:/tmp/dockertests tf-exp-devel /devel.build_addons.sh
--- experimental_official_dockerfiles/master/devel.build_tf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-source ~/.bashrc
-pyenv global tf-38
-mv devel.bazelrc /tmp/dockertests/tensorflow/.tf_configure.bazelrc
-cd /tmp/dockertests/tensorflow
-bazel build tensorflow/tools/pip_package:build_pip_package --disk_cache=/tmp/dockertests/cache
-./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/dockertests/pkg --gpu
