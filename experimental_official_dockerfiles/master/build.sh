5c548e5cc897fb95ec803efbaea83faed759291b
Austin Anderson <angerson@google.com>
2021-05-06 11:37:24 -0700

    Fix a few breakages

--- experimental_official_dockerfiles/master/build.sh
+++ experimental_official_dockerfiles/master/build.sh
@@ -5,5 +5,5 @@
 # tensorflow - tensorflow source code
 set -euxo pipefail
 DOCKER_BUILDKIT=1 docker build --target=devel --build-arg PYTHON_VERSION=python3.8 -t tf-exp-devel .
-docker run --rm -v ~/tmp/dockertests:/tf tf-exp-devel bats -T /devel.preliminary.bats
-docker run --rm -v ~/tmp/dockertests:/tf tf-exp-devel /devel.build_tf.sh
+docker run --rm -v ~/tmp/dockertests:/tf tf-exp-devel bats -T /user_tools/preliminary.bats
+docker run --rm -v ~/tmp/dockertests:/tf tf-exp-devel /user_tools/build_tf.sh
--- experimental_official_dockerfiles/master/devel.user_tools/build_tf.sh
+++ experimental_official_dockerfiles/master/devel.user_tools/build_tf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 cd /tf/tensorflow
-bazel build tensorflow/tools/pip_package:build_pip_package --bazelrc=/user_tools/gpu.bazelrc --disk_cache=/tf/cache
+bazel --bazelrc=/user_tools/gpu.bazelrc build tensorflow/tools/pip_package:build_pip_package  --disk_cache=/tf/cache
 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tf/pkg --gpu
