81b4d54a4d796d5cb41307765ac5ca5c7c3ed0b4
Austin Anderson <angerson@google.com>
2021-06-15 11:23:06 -0700

    Fix script and update to JDK 11

--- official_build_environments/master/linux/devel.user_tools/rename_and_verify_wheel.sh
+++ official_build_environments/master/linux/devel.user_tools/rename_and_verify_wheel.sh
@@ -4,5 +4,5 @@ set -e
 # Check and rename the wheel with auditwheel. Inserts the platform tags like
 # "manylinux_xyz" into the wheel filename.
 TF_WHEEL=$(find /tf/pkg -iname '*.whl')
-python3 -m auditwheel repair --plat manylinux2010_x86_64 "$TF_WHEEL" --wheel-dir /tf/pkg | tee check.txt
+python3 -m auditwheel repair --plat manylinux2010_x86_64 "$TF_WHEEL" --wheel-dir /tf/pkg 2>&1 | tee check.txt
 if grep -q "Fixed-up wheel written to" check.txt; then rm $TF_WHEEL; fi
