b9ee7a33f0449d9d0bf534a2a229d8a1bd41c82e
Ramesh Sampath <rameshsampath@google.com>
2021-12-09 13:51:02 -0800

    test pip

--- tf_sig_build_dockerfiles/setup.python.sh
+++ tf_sig_build_dockerfiles/setup.python.sh
@@ -33,9 +33,16 @@ ln -sf /usr/bin/$VERSION /usr/bin/python
 ln -sf /usr/lib/$VERSION /usr/lib/tf_python
 
 # Install pip
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
-python3 -m pip install --no-cache-dir pip~=21.2.0
+if [[ "$VERSION" == "python3.8" ]]; then
+  # Python 3.10 pip reference is broken for pip 21.3 (https://github.com/pypa/pip/issues/10647)
+  # Don't update the pip after installed with ensurepip
+  # TODO(rameshsampath): Remove once Python 3.10 works with latest pip
+  python3 -m ensurepip
+else
+  curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+  python3 get-pip.py
+  python3 -m pip install --no-cache-dir --upgrade pip
+fi
 
 # Disable the cache dir to save image space, and install packages
 python3 -m pip install --no-cache-dir -r $REQUIREMENTS -U
