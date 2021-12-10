ca384bd5868e058dff735d57cda19e66ecdefce6
Austin Anderson <angerson@google.com>
2021-12-10 11:26:32 -0800

    Always pass and be quiet

--- tf_sig_build_dockerfiles/devel.usertools/get_test_list.sh
+++ tf_sig_build_dockerfiles/devel.usertools/get_test_list.sh
@@ -2,7 +2,7 @@
 
 # Usage: get_test_list.sh OUTPUT BAZEL_TEST_COMMAND...
 # Writes the list of tests that would be run from BAZEL_TEST_COMMAND to OUTPUT.
-set -euo pipefail
+# Hides all extra output and always exits with success for now.
 OUTPUT=$1
 shift
-"$@" --check_tests_up_to_date | sort -u | awk '{print $1}' | grep "^//" | tee $OUTPUT
+"$@" --check_tests_up_to_date 2>/dev/null | sort -u | awk '{print $1}' | grep "^//" | tee $OUTPUT
