ac41fe7468dae1577954f76ebbb8b3631441347e
Austin Anderson <angerson@google.com>
2022-04-14 15:05:09 -0700

    Fix summary generator and disable clang

--- tf_sig_build_dockerfiles/devel.usertools/save_summary_xml.sh
+++ tf_sig_build_dockerfiles/devel.usertools/save_summary_xml.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Usage: save_summary.sh OUTPUT BAZEL_TEST_COMMAND...
+# Usage: save_summary_xml.sh OUTPUT BAZEL_TEST_COMMAND...
 OUTPUT=$1
 shift
 mkdir -p "$(dirname $OUTPUT)"
-"$@" 2>&1 | tee $OUTPUT.txt
-/usertools/save_summary_xml.sh $OUTPUT.txt $OUTPUT
+"$@" 2>&1 | tee "$OUTPUT.txt"
+python /usertools/save_summary_xml.py $OUTPUT.txt $OUTPUT
 rm $OUTPUT.txt
