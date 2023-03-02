7c9eb4249d212f86de52bab1d9be6da378b6a399
halseycamilla <halseycamilla@google.com>
2023-03-02 08:14:25 +0000

    Change directory name

---
+++ utils/build_stats_functions/build_profile/profile_test.py
@@ -0,0 +1,55 @@
+# Copyright 2023 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+import os
+import profile
+import pytest
+
+
+os.environ['STORAGE_BUCKET'] = 'processing_files_build_logs'
+os.environ['TABLE_ID'] = 'tensorflow-devel.build_profile_stats'
+
+def test_check_path():
+  # Path for where to find the build profiles we want in GCS storage
+  r1 = profile.check_path(
+      'resultstore/prod/tensorflow/rel/profile.json'
+  )
+  r2 = profile.check_path(
+      'resultstore/prod/tensorflow/rel/random_folder/profile.json'
+  )
+  r3 = profile.check_path(
+       'resultstore/prod/tensorflow/rel.*/build.json'
+  )
+  assert r1 is not None
+  assert r2 is not None
+  assert r3 is None
+
+def test_get_data():
+  f1 = '/usr/local/google/home/halseycamilla/pytest/build/utils/build_stats/testing_files/no_first_line.json'
+  with pytest.raises(profile.MissingTopLevelError):
+    profile.get_data(f1)
+  no_tid = '/usr/local/google/home/halseycamilla/pytest/build/utils/build_stats/testing_files/no_thread_id.json'
+  with pytest.raises(profile.IncorrectProfileFormatError):
+    profile.get_data(no_tid)
+  no_name = '/usr/local/google/home/halseycamilla/pytest/build/utils/build_stats/testing_files/no_event_name.json'
+  assert profile.get_data(no_name) is not None
+
+def test_get_percents():
+  no_name = '/usr/local/google/home/halseycamilla/pytest/build/utils/build_stats/testing_files/no_event_name.json'
+  with pytest.raises(profile.IncorrectProfileFormatError):
+    profile.get_percents(profile.get_data(no_name))
+  no_ts = '/usr/local/google/home/halseycamilla/pytest/build/utils/build_stats/testing_files/no_timestamp.json'
+  with pytest.raises(profile.IncorrectProfileFormatError):
+       profile.get_percents(profile.get_data(no_ts))
+
