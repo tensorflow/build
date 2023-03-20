fcafed4d6971bed3cca5910583bf75cc4df9f3b9
Austin Anderson <angerson@google.com>
2023-03-20 16:52:37 -0700

    Ignore missing old artifact

--- tf_oss_dashboard/script.sh
+++ tf_oss_dashboard/script.sh
@@ -4,6 +4,10 @@ set -euxo pipefail
 pip install -r requirements.txt
 
 ./query_api.sh > new.json
-./merge.py old.json new.json > merged.json
+if [[ -e old.json ]]; then
+  ./merge.py old.json new.json > merged.json
+else
+  mv new.json merged.json
+fi
 cat merged.json ./dashboard.py > dashboard.html
 mv merged.json old.json
