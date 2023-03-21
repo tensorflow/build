e5a16c640344249a6d64840f3173cd62126a633e
Austin Anderson <angerson@google.com>
2023-03-21 16:54:24 -0700

    Logging fixes

--- tf_oss_dashboard/script.sh
+++ tf_oss_dashboard/script.sh
@@ -4,19 +4,19 @@ set -euxo pipefail
 pip install -r requirements.txt
 
 ./query_api.sh > new.json
-echo ::group::New Query Data
+echo "::group::New Query Data"
 cat new.json
-echo ::endgroup::
+echo "::endgroup::"
 if [[ -e "../Merged Data/old.json" ]]; then
   mv "../Merged Data/old.json" old.json
   ./merge.py old.json new.json > merged.json
-  echo ::group::Merged Data
+  echo "::group::Merged Data"
   cat merged.json
-  echo ::endgroup::
+  echo "::endgroup::"
 else
   mv new.json merged.json
 fi
-echo ::group::Dashboard Generator Output
+echo "::group::Dashboard Generator Output"
 cat merged.json | ./dashboard.py | tee dashboard.html
-echo ::endgroup::
+echo "::endgroup::"
 mv merged.json old.json
