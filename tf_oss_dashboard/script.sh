3ececd42df778cee72b4a294b8baf6f51d56652c
Austin Anderson <angerson@google.com>
2023-03-20 17:10:38 -0700

    Add debug logs

--- tf_oss_dashboard/script.sh
+++ tf_oss_dashboard/script.sh
@@ -4,10 +4,13 @@ set -euxo pipefail
 pip install -r requirements.txt
 
 ./query_api.sh > new.json
+cat new.json
 if [[ -e old.json ]]; then
   ./merge.py old.json new.json > merged.json
+  echo "MERGED"
+  cat merged.json
 else
   mv new.json merged.json
 fi
-cat merged.json | ./dashboard.py > dashboard.html
+cat merged.json | ./dashboard.py | tee dashboard.html
 mv merged.json old.json
