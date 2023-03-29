3a70120db834af1dc53c45244f13b4b396907d20
halseycamilla <111537937+halseycamilla@users.noreply.github.com>
2023-03-29 07:40:15 -0700

    Update profile.py

--- utils/build_stats_functions/build_profile/profile.py
+++ utils/build_stats_functions/build_profile/profile.py
@@ -14,9 +14,10 @@
 # =============================================================================
 """Python Code for grabbing stats from build profile to import into BigQuery."""
 import json
-import logging
 import os
 import re
+from os import path
+import gzip
 
 import functions_framework
 from google.cloud import bigquery
@@ -39,9 +40,7 @@ class IncorrectProfileFormatError(Exception):
 @functions_framework.cloud_event
 def main(cloud_event: any):
   """Entry point function that is triggered by uploading build profile.
-  Gathers stats from build profile file and sends to BigQuery. Goes through
-  each thread in the file and for each event in the thread extracts relevant
-  info and writes as an object into bigquery
+  Gathers stats from build profile and uploads to Bigquery.
   Args:
     cloud_event: the google cloud event that triggered the function
   """
@@ -49,38 +48,40 @@ def main(cloud_event: any):
   TABLE_ID = os.environ.get("TABLE_ID")
   try:
     client = logging.Client()
-    log_name = "trying-this-out"
+    log_name = "build-profile-testing"
     logger = client.logger(log_name)
     logger.log_text("Successfully connected to GCP Logging Client", severity="INFO")
   except logging.Error:
     logger.log_text("Unable to connect to GCP Logging client", severity="WARNING")
     return
   data = cloud_event.data
-  if "name" not in data:
+  if "name" not in data: 
     logger.log_text("No filename was found", severity="WARNING")
     return
   file_name = data["name"]
-  print(file_name)
   check_file = check_path(file_name)
   if not check_file:
     logger.log_text(
-        "The current file is not a build profile according to its path", severity="INFO"
+        "The current file is not a Build profile according to its path", severity="INFO"
     )
     return
   else:
+    # Extract job name from overall path
     logger.log_text("Build profile found", severity="INFO")
+    r = re.search("/[0-9].*", file_name)
+    index = r.span()[0]
+    job = file_name[0:index]
   try:
     storage_client = storage.Client()
-    print(STORAGE_BUCKET)
     bucket = storage_client.get_bucket(STORAGE_BUCKET)
-    print(bucket)
     data_blob = bucket.get_blob(file_name)
-    print(data_blob)
   except Exception as exc:
     logger.log_text("Unable to access Cloud Storage client or storage bucket", severity="WARNING")
     raise Exception() from exc
-  threads = get_data(data_blob)
-  all_threads = get_percents(threads)
+  results = get_data(data_blob)
+  build_id = results[0]
+  threads = results[1]
+  all_threads = get_times(threads)
   if len(all_threads) == 0:
     logger.log_text(
         "Profile had no completed action events or wasn't in correct format", severity="WARNING"
@@ -90,9 +91,11 @@ def main(cloud_event: any):
   try:
     bigquery_client = bigquery.Client()
     for obj in objs:
+      obj["BUILD_ID"] = build_id
+      obj["JOB_NAME"] = job
       errors = bigquery_client.insert_rows_json(TABLE_ID, [obj])
       if not errors:
-        logger.log_text("New rows have been added.", severity="INFO")
+        logger.log_text("Successfully added row to table", severity="INFO")
       else:
         logger.log_text("Encountered errors while inserting rows", severity="INFO")
   except bigquery.Error:
@@ -100,7 +103,14 @@ def main(cloud_event: any):
     return
 
 def check_path(file_name: str):
-  regex = "resultstore/prod/tensorflow/rel.*/profile.json"
+  """Checks that the uploaded file is a BEP using regex
+  Args:
+    file_name: file path name
+  Returns:
+    The first occurence in the path that matches regex 
+    or None if no match
+  """
+  regex = "prod/tensorflow/rel.*/profile.json.gz"
   check_file = re.match(regex, file_name)
   return check_file
 
@@ -116,24 +126,34 @@ def get_data(data_blob: any):
   Returns:
     A dict mapping keys of thread ids to an array of events in that thread
   """
+  # Unzip file
+  root = path.dirname(path.abspath(__file__))
+  root = root + "/profile.json.gz"
+  data_blob.download_to_filename(root)
   threads = {}
-  with data_blob.open("r") as file:
+  with gzip.open(root, "rb") as file:
     i = 0
     for line in file:
+      line = line.decode('utf-8')
       if i == 0:
         # Make sure the first line is top level object containing
         # MetaData "Otherdata" and "TraceEvents"
         if "otherData" not in line or "traceEvents" not in line:
           raise MissingTopLevelError()
           logger.log_text("Missing first line of profile", severity="WARNING")
+        # Remove the "traceEvents" and outstanding "[" in top level to be able
+        # to parse it as a Json
+        line = line[0:len(line)-17] + "}"
+        line = json.loads(line)
+        if "build_id" not in line["otherData"]:
+          raise IncorrectProfileFormatError()
+        build_id = line["otherData"]["build_id"]
       if i > 0:
         my_json = line.rstrip()
         if my_json[-1] == ",":
           my_json = my_json[0 : len(my_json) - 1]
         if my_json[-1] != "}":
           break
-      # JSONDecodeError is general error for incorrect Json Format
-      # Wrap in class error to have more specific exception
         try:
           data = json.loads(my_json)
         except ValueError as exc:
@@ -145,11 +165,14 @@ def get_data(data_blob: any):
           threads[a] = []
         threads[a].append(data)
       i += 1
-  return threads
+  res = [build_id, threads]
+  return res
+      
 
 
-def get_percents(threads: dict):
-  """For each event in thread keep track of total time it takes.
+def get_times(threads: dict):
+  """For each event in thread keep track of total time it takes
+  and its category
   Args:
     threads: dictionary of {thread id: [events in thread]}
   Raises:
@@ -160,25 +183,29 @@ def get_percents(threads: dict):
   # For each thread create a dictionary of {eventtype: sum of eventtype times}
   # Also keep an overall dictionary of all of the threads
   all_threads = {}
+  categories = {}
+  build_time = 0 
   for thread in threads:
     total_time = 0
     all_events = []
     event_times = {}
     seen = set()
     for event in threads[thread]:
-      if "ts" not in event or "dur" not in event or "ph" not in event:
+      if "ph" not in event:
         raise IncorrectProfileFormatError()
-      # If the current event is a complete event (indicated by phase X)
-      # we want to look at it because then it has all the information we need
       if event["ph"] == "X":
-        # To calculate the self time of the event want to get the duration
-        # and subtract the time of any child jobs
+        if "ts" not in event or "dur" not in event:
+          raise IncorrectProfileFormatError()
+      # Only look at complete events (indicated by phase X)
+        # To calculate self time want to get the duration
+        # and subtract the time of any child jobs.
         # Child events show up before the current event however can be out of 
-        # order overall so hence loop through all previous events in the file
-        # if the time stamps overlap we see it is a child event
+        # order overall so hence loop through all previous events in the file.
+        # If the time stamps overlap we see it is a child event.
         # NOTE: In rare occassions there won't be an overlap showing up but 
         # this only throws off overall self time of event by 10ths of a second.
         self_time = event["dur"]
+        categories[event["name"]] = event["cat"]
         if total_time > 0:
           low = event["ts"]
           high = low + event["dur"]
@@ -194,24 +221,31 @@ def get_percents(threads: dict):
               self_time -= curr["dur"]
         all_events.append(event)
         total_time += self_time
-        if "name " not in event:
+        if "name" not in event:
           raise IncorrectProfileFormatError()
         event_type = event["name"]
-        event_type = event_type.replace(":", "")
-        event_type = event_type.replace(" ", "")
         if event_type not in event_times:
           event_times[event_type] = 0
         event_times[event_type] += self_time
     all_threads[thread] = event_times
-  return all_threads
+  return [all_threads, categories]
 
-def create_event_objects(all_threads: dict):
+def create_event_objects(data: list):
+  """For each event in each thread create an object
+  Args:
+    data: list of threads and categories
+  Returns:
+    A list of all events as objects
+  """
+  all_threads = data[0]
+  categories = data[1]
   objs = []
   for line in all_threads:
     for data in all_threads[line]:
       ev = {}
+      ev["EVENT_NAME"] = data
+      ev["CATEGORY"] = categories[data]
       ev["THREAD"] = line
-      ev["EVENT_TYPE"] = data
-      ev["TOTAL_TIME"] = all_threads[line][data]
+      ev["TIME_TAKEN"] = float(all_threads[line][data])/1000000
       objs.append(ev)
   return objs
