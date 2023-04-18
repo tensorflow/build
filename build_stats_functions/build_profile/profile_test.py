a3eca41dbe31dd952af2c63813b346a80c6031ee
halseycamilla <111537937+halseycamilla@users.noreply.github.com>
2023-04-18 13:19:34 -0700

    Update profile_test.py

--- build_stats_functions/build_profile/profile_test.py
+++ build_stats_functions/build_profile/profile_test.py
@@ -33,20 +33,20 @@ def test_check_path():
   assert r3 is None
 
 def test_get_data():
-  f1 = '//build/build_stats_functions/testing_files/no_first_line.json'
+  f1 = '../testing_files/no_first_line.json'
   with pytest.raises(profile.MissingTopLevelError):
     profile.get_data(f1)
-  no_tid = '//build/build_stats_functions/testing_files/no_thread_id.json'
+  no_tid = '../testing_files/no_thread_id.json'
   with pytest.raises(profile.IncorrectProfileFormatError):
     profile.get_data(no_tid)
-  no_name = '//build/build_stats_functions/testing_files/no_event_name.json'
+  no_name = '../testing_files/no_event_name.json'
   assert profile.get_data(no_name) is not None
 
 def test_get_percents():
-  no_name = '//build/build_stats_functions/testing_files/no_event_name.json'
+  no_name = '../testing_files/no_event_name.json'
   with pytest.raises(profile.IncorrectProfileFormatError):
     profile.get_percents(profile.get_data(no_name))
-  no_ts = '//build/build_stats_functions/testing_files/no_timestamp.json'
+  no_ts = '../testing_files/no_timestamp.json'
   with pytest.raises(profile.IncorrectProfileFormatError):
        profile.get_percents(profile.get_data(no_ts))
 
