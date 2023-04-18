5e0ba5aa017fa24ec528fb1a10afe9708ba2aa87
Austin Anderson <angerson@google.com>
2023-04-18 10:54:40 -0700

    Fix reversing

--- tf_oss_dashboard/dashboard.py
+++ tf_oss_dashboard/dashboard.py
@@ -71,7 +71,7 @@ by_name = defaultdict(list)
 for record in records:
   by_name[record["name"]].append(record)
 
-nightlies = by_name["Nightly - Code Check - Linux"].reverse()
+nightlies = reversed(by_name["Nightly - Code Check - Linux"])
 for name, jobs in by_name.items():
   for job in jobs:
     for nightly in nightlies:
