83f6ef5900a7ce3f73db03c0cb961de8eb6e3ce1
Austin Anderson <angerson@google.com>
2023-04-20 15:22:47 -0700

    Remove favorites, clean up js

--- tf_oss_dashboard/script.js
+++ tf_oss_dashboard/script.js
@@ -1,142 +1,110 @@
-//////////////////////////////////////////////////////////////////////////////
-// HTML BODY STYLE TOGGLES
-//////////////////////////////////////////////////////////////////////////////
-// Set styles upon page load
-if (localStorage.getItem('cb') == 'true') {
-  $("#colorblind").prop("checked", true)
-  $("body").toggleClass("colorblind")
+// Navbar toggle switches
+// When the page loads or when the navbar toggles are switched, toggle the
+// class on the HTML <body> tag that controls those features (colorblind or
+// show-the-build-cop-section support).
+if (localStorage.getItem('tf-colorblind') == 'true') {
+  $('#colorblind').prop('checked', true)
+  $('body').toggleClass('colorblind')
 }
-if (localStorage.getItem('showfailures') == 'true') {
-  $("#showfailures").prop("checked", true)
-  $("body").toggleClass("showfailures")
+if (localStorage.getItem('tf-showbuildcop') == 'true') {
+  $('#showbuildcop').prop('checked', true)
+  $('body').toggleClass('showbuildcop')
 }
-// Set styles when navbar items toggled
-$("#colorblind").change(function(e) {
-  localStorage.setItem('cb', $(this).prop('checked'))
-  $("body").toggleClass("colorblind")
+// Note: $(this) in jQuery yields the element upon which this event is called
+$('#colorblind').change(function () {
+  localStorage.setItem('tf-colorblind', $(this).prop('checked'))
+  $('body').toggleClass('colorblind')
 })
-$("#showfailures").change(function(e) {
-  localStorage.setItem('showfailures', $(this).prop('checked'))
-  $("body").toggleClass("showfailures")
+$('#showbuildcop').change(function () {
+  localStorage.setItem('showbuildcop', $(this).prop('checked'))
+  $('body').toggleClass('showbuildcop')
 })
 
-//////////////////////////////////////////////////////////////////////////////
-// AUTO REFRESH AND TIMESTAMP HANDLING
-//////////////////////////////////////////////////////////////////////////////
-let autorefreshready = false
-let modal_is_open = false;
-function humanizeTimestamp() {
-  if (!modal_is_open && autorefreshready) {
-    location.reload()
-  }
-  let str = moment($('#tf-now').attr("data-isonow"), moment.ISO_8601).fromNow()
-  $('#tf-ago').text("(" + str + ")")
+// Update the human-readable timestamp once per minute and when the page loads
+// Uses moment.js, which is included in the top of the HTML page
+function humanizeTimestamp () {
+  const time_ago = moment($('#tf-now').attr('data-isonow'), moment.ISO_8601).fromNow()
+  $('#tf-ago').text(`(${time_ago})`)
 }
-function setAutoRefreshReady() {
-  autorefreshready = true
-}
-setInterval(setAutoRefreshReady, 300000) // Every 5 mins, refresh unless modal is open
-setInterval(humanizeTimestamp, 60500) // Every 1 min, update the timestamp
+setInterval(humanizeTimestamp, 60500)
 humanizeTimestamp()
 
-//////////////////////////////////////////////////////////////////////////////
+// Check every minute if it's time to refresh the page, which is controlled
+// by a flag that gets set after five minutes. Delay the refresh if there is
+// currently a modal open, because that means someone is probably interacting
+// with the page right now and would be annoyed their view changed (because of
+// the way the line highlighting works, no job is highlighted when the page
+// refreshes, even though the same modal pops up).
+let autoRefreshASAP = false
+let modalIsOpen = false
+function refreshIfNeeded () {
+  if (!modalIsOpen && autoRefreshASAP) {
+    location.reload()
+  }
+}
+setInterval(refreshIfNeeded, 60500)
+setInterval(function () { autoRefreshASAP = true }, 300000)
+
 // MODAL HANDLING
+//
+// We hook into the modals' open and close events to track whether any modal
+// is open (to avoid refreshing the page, see above) and to highlight the
+// clicked-on job (which is a very nice quality-of-life feature).
+//
+// See:
+//  - https://getbootstrap.com/docs/5.2/components/modal/#via-javascript
+//  - https://getbootstrap.com/docs/5.2/components/modal/#methods
+//  - https://getbootstrap.com/docs/5.2/components/modal/#events
+//
 // NOTE: These all go into jQuery's $(function() {}), which executes the content
 // only after the whole page has loaded and the DOM is ready. This allows us
 // to have all of the scripts in one file: since the script is loaded just
 // after the navbar is placed, the code above can modify the page for styles
 // and card placement before the cards and modals (which take up most of the
 // page size) are loaded. This means there's generally no flashing when the page
-// loads, except to reload the favorites, which seems unavoidable.
-//////////////////////////////////////////////////////////////////////////////
-$(function() {
-
-  //////////////////////////////////////////////////////////////////////////////
-  // FAVORITES HANDLING
-  //////////////////////////////////////////////////////////////////////////////
-  function reorder() {
-    let favorites = JSON.parse(localStorage.getItem('favorites') || '{}')
-    $('.favorites-section .card-section').empty()
-    $('.favorite').each(function() {
-      let current = $(this).closest('.card').attr("data-name")
-      if (favorites[current]) {
-        $(this).addClass("favorited")
-        $(this).closest('.card').clone(true).appendTo('.favorites-section .card-section')
-      } else {
-        $(this).removeClass("favorited")
-      }
-    })
-    if ($('.favorites-section .card-section').children().length > 0) {
-      $('.favorites-section').removeClass("d-none")
-    } else {
-      $('.favorites-section').addClass("d-none")
-    }
-  }
-
-  $('.favorite').on('click', function() {
-    let current = $(this).closest('.card').attr("data-name")
-    let favorites = JSON.parse(localStorage.getItem('favorites') || '{}')
-    if (favorites[current] !== null) {
-      favorites[current] = !favorites[current]
-    } else {
-      favorites[current] = true
-    }
-    localStorage.setItem('favorites', JSON.stringify(favorites))
-    reorder()
-  })
-
-  let favorites = JSON.parse(localStorage.getItem('favorites') || '{}')
-  $('.favorite').each(function() {
-    let current = $(this).closest('.card').attr("data-name")
-    if (favorites[current]) {
-      $(this).toggleClass("favorited")
-    }
-  })
-  reorder()
-
+// loads.
+$(function () {
   // Highlight the clicked job when a modal appears (doesn't work when a commit
   // is linked directly).
-  $(".commit-modal").on('show.bs.modal', function(e) {
-    modal_is_open = true;
-    let source = e.relatedTarget
-    name = $(source).closest('.card').attr("data-name")
-    $(this).find("td span").filter(function() {
-      return $(this).text() === name
-    }).closest("tr").toggleClass("tf-table-highlight")
-    $(this).attr("data-tf-trigger", name)
+  $('.commit-modal').on('show.bs.modal', function (e) {
+    modalIsOpen = true
+    const clicked_square = e.relatedTarget
+    job_name = $(clicked_square).closest('.card').attr('data-name')
+    $(this).find('td span').filter(function () {
+      return $(this).text() === job_name
+    }).closest('tr').toggleClass('tf-table-highlight')
+    $(this).attr('data-tf-job-name', job_name)
 
     // Set the window location hash to this commit ID without corrupting history
-    history.replaceState(undefined, undefined, "#" + $(this).attr("id"))
+    history.replaceState(undefined, undefined, '#' + $(this).attr('id'))
   })
 
   // Undo the previous when the modal is hidden
-  $(".commit-modal").on('hidden.bs.modal', function(e) {
-    modal_is_open = false;
-    let name = $(this).attr("data-tf-trigger") 
-    $(this).find("td span").filter(function() {
-      return $(this).text() === name
-    }).closest("tr").toggleClass("tf-table-highlight")
-    // Set the window location hash to nothing (remove the #...)
-    history.replaceState(null, null, ' ');
+  $('.commit-modal').on('hidden.bs.modal', function (e) {
+    modalIsOpen = false
+    const job_name = $(this).attr('data-tf-job-name')
+    $(this).find('td span').filter(function () {
+      return $(this).text() === job_name
+    }).closest('tr').toggleClass('tf-table-highlight')
+    // Set the window location hash to nothing (remove the #...) without
+    // corrupting history
+    history.replaceState(null, null, ' ')
   })
 
   // When the page loads, if there is a commit ID in the location hash, show
   // the matching modal -- that is, if you load dashboard#commit, show the modal
-  // for that commit.
+  // for that commit. Also, show the matching modal if a CL is provided instead.
   if (window.location.hash.length <= 1) {
-    // Nothing to do
+    // Nothing to do if no hash in the URL
+  // If the hash is exactly 41 chars (hash sign # plus a 40-char sha hash),
+  // just show that modal.
   } else if (window.location.hash.length == 41) {
     new bootstrap.Modal(window.location.hash).show()
-  // And if it's not a commit sha (which is always 40 chars + a hash symbol),
-  // try matching against PR number...
-  } else if (window.location.hash.length < 10) {
-    let pr = $(".modal p:first-child").filter(function() {
-      return $(this).text().indexOf("Merge pull request " + window.location.hash) >= 0;
-    })
-    new bootstrap.Modal("#" + pr.closest(".modal").attr('id')).show()
-  // And if it's a CL, try and find a modal matching that CL number
+  // And if it's not a commit sha, it's a CL, so try and find a modal
+  // matching that CL number
   } else {
-    let cl = $(".modal[data-cl=" + window.location.hash.substring(1) + "]")
-    new bootstrap.Modal("#" + cl.attr('id')).show()
+    const cl = window.location.hash.substring(1)
+    const modal_id = '#' + $(`.modal[data-cl=${cl}]`).attr('id')
+    new bootstrap.Modal(modal_id).show()
   }
 })
