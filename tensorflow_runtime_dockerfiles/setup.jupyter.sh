830ebce8a7bca058e9d8868c1e2521b7ed785e0e
Oliver Ofenloch <57812959+ofenloch@users.noreply.github.com>
2024-04-02 21:35:59 +0200

    use environment variable TENSORFLOW_NOTEBOOK_DIR to create the tutorials directory

--- tensorflow_runtime_dockerfiles/setup.jupyter.sh
+++ tensorflow_runtime_dockerfiles/setup.jupyter.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 jupyter serverextension enable --py jupyter_http_over_ws
 
-mkdir -p /tf/tensorflow-tutorials
-chmod -R a+rwx /tf/
+TENSORFLOW_TUTORIALS="${TENSORFLOW_NOTEBOOK_DIR}/tensorflow-tutorials"
+mkdir -p -v ${TENSORFLOW_TUTORIALS}
+
+chmod -R a+rwx ${TENSORFLOW_NOTEBOOK_DIR}
 mkdir /.local
 chmod a+rwx /.local
 apt-get update
 apt-get install -y --no-install-recommends wget git
-cd /tf/tensorflow-tutorials
+cd ${TENSORFLOW_TUTORIALS}
 wget https://raw.githubusercontent.com/tensorflow/docs/master/site/en/tutorials/keras/classification.ipynb
 wget https://raw.githubusercontent.com/tensorflow/docs/master/site/en/tutorials/keras/overfit_and_underfit.ipynb
 wget https://raw.githubusercontent.com/tensorflow/docs/master/site/en/tutorials/keras/regression.ipynb
